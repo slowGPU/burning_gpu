39f346b7a3a3d99146ad78083c04490f76ff746c
MoonRainy21 <shmoon232@snu.ac.kr>
2024-08-13 07:02:47 +0000

    EDA and script changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import mlflow
 from lightning.pytorch.cli import LightningCLI
 
-from dataset import SliceDataModule
+from dataset import SliceDataModule, GrappaDataModule, SliceGrappaDataModule
 from models import *  # noqa: F403
 
 mlflow.autolog()
 
-cli = LightningCLI(datamodule_class=SliceDataModule)
+cli = LightningCLI()
--- train.sh
+++ train.sh
@@ -1,8 +1,9 @@
 python main.py fit \
     --trainer.max_epochs 15 \
     --seed_everything 42 \
-    --model 'VarNetLogisticUnetSensFixOL' \
-    --model.num_cascades 4 \
-    --model.sens_chans 4 \
+    --model 'VarNetFreezedSensNAFNetOL' \
+    --model.sens_net_path 'checkpoints/sens_net/VarNetLogisticUnetSensFix.pt' \
+    --model.num_cascades 10 \
+    --model.nafnet_width 16 \
+    --data 'SliceGrappaDataModule' \
     --data.root '/home/Data' \
-  
